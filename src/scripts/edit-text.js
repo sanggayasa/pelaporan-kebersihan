7b144fa1d10e71c9026719b6e3ac912fd252cd2b
sanggayasa <sanggayasa502@gmail.com>
2021-11-24 07:19:47 +0700

    “first”

---
+++ src/scripts/edit-text.js
@@ -0,0 +1,6 @@
+const editText = function () {
+  const judul = document.querySelector('h1');
+  judul.innerHTML = 'JUDUL kedua';
+};
+
+export default editText;
---
+++ src/scripts/index.js
@@ -0,0 +1,10 @@
+/*
+masukan modul dengan format seperti ini
+import modul_name from 'module_lookasi';
+*/
+
+import '../styles/style.css';
+import editText from './edit-text';
+import 'regenerator-runtime';
+
+editText();
