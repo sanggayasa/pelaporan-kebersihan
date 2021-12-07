db1c23e3663c1ce1720b9bd8d8b6d031cbc4a03a
M Alpi Syahdan <alpisyahdan11@gmail.com>
2021-12-07 21:18:49 +0700

    Menambahkan File index.js

---
+++ src/index.js
@@ -0,0 +1,8 @@
+/* eslint-disable semi */
+/* eslint-disable eol-last */
+/* eslint-disable import/extensions */
+import "regenerator-runtime";
+import "bootstrap/dist/js/bootstrap.min.js";
+import main from "./scripts/main";
+
+document.addEventListener("DOMContentLoaded", main);
